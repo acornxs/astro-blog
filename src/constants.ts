34de9ecf5f85b8243259b690f3f020cd88cec2b5
acornxs <xs@acorn.xin>
2026-01-30 23:39:12 +0800

    移除网站的Social Links，只保留邮箱

--- src/constants.ts
+++ src/constants.ts
@@ -17,7 +17,7 @@ interface Social {
 }
 
 export const SOCIALS: Social[] = [
-  {
+  /* {
     name: "GitHub",
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: `${SITE.title} on GitHub`,
@@ -34,7 +34,7 @@ export const SOCIALS: Social[] = [
     href: "https://www.linkedin.com/in/username/",
     linkTitle: `${SITE.title} on LinkedIn`,
     icon: IconLinkedin,
-  },
+  }, */
   {
     name: "Mail",
     href: "mailto:xs@acorn.xin",
